1fe83b195ebe83dbaec4c7c63614a3b88e4a900f
David <djsy1414@gmail.com>
2024-10-03 20:49:12 +0800

    feat: add progress bar; complete result text_area

--- main.py
+++ main.py
@@ -8,12 +8,28 @@ format = st.text_input("Format:", value='mp4')
 folder_path = st.text_input("Folder path:", value='downloaded_files')
 file_name = st.text_input("File name (leave it empty if you'd like to use default file name):")
 
+if "text_area" not in st.session_state:
+    st.session_state["text_area"] = ""
+
+st.text_area("Result", value=st.session_state["text_area"], height=200)
+bar = st.progress(0)
+
 def trigger_download():
-    dy = DownloadYoutube(link=link, format=format.lower(), file_name=file_name, resolution=resolution.lower(), default_folder=folder_path)
-    st.text_area('Result : ', 
-                 f"""title: {dy.yt.title} \n length of the video: {st.yt.length} \n""")
-    # dy.download()
-    print(link, resolution, format, folder_path, file_name)
+    dy = DownloadYoutube(link=link, format=format.lower(), file_name=file_name, resolution=resolution.lower(), default_folder=folder_path, progress_bar=bar)
+    if dy.is_error:
+        st.session_state["text_area"] = f"Error: {dy.error_msg}"
+    else:
+        dy.connect_yt()
+        if dy.is_error:
+            st.session_state["text_area"] = f"Error: {dy.error_msg}"
+        else:
+            st.session_state["text_area"] = f"{dy.display_msg}"
+            dy.download()
+            if dy.is_error:
+                st.session_state["text_area"] = f"Error: {dy.error_msg}"
+            else:
+                st.session_state["text_area"] = f"{dy.display_msg}"
+
 
 st.button(label='Download', on_click=trigger_download)
 
--- youtube.py
+++ youtube.py
@@ -1,51 +1,88 @@
 # from pytube import YouTube
 from pytubefix import YouTube
-from pytubefix.cli import on_progress
+# from pytubefix.cli import on_progress
+import streamlit as st
 import os
-
-def onProgress(stream, chunk, remains):
-    total = stream.filesize                     # get file size
-    percent = (total-remains) / total * 100     # calculate the remain percentage
-    print(f'Downloading… {percent:05.2f}%')  
 
 
 class DownloadYoutube():
-    def __init__(self, link, format='mp4', file_name=None, resolution='highest', default_folder='downloaded_files'):
+    def __init__(self, link, format='mp4', file_name=None, resolution='highest', default_folder='downloaded_files', progress_bar=None):
         self.link = link
         self.format = format
         self.file_name = file_name
         self.resolution = resolution
         self.default_folder = default_folder
+        self.progress_bar = progress_bar
         self.is_error = False
         self.error_msg = ""
+        self.display_msg = ""
 
+        self.check_default_value()
+
+
+    def check_default_value(self):
+        # check if the link is valid
+        if self.link is None or self.link == "":
+            self.is_error = True
+            self.error_msg += 'link is empty'
+
+        # check if the format is valid
+        allowed_format_list = ["mp4", "mp3"]
+        if self.format not in allowed_format_list:
+            self.is_error = True
+            self.error_msg += f'format should be one of options: {",".join(allowed_format_list)}. Find {self.format}' 
+
+        # check if the default_folder exists
         if not os.path.isdir(self.default_folder):
             os.mkdir(self.default_folder)
 
-        self.yt = YouTube(self.link, on_progress_callback=on_progress)
-
-        print(f"title: {self.yt.title}")
-        print(f"length of the video: {self.yt.length}") 
-        print(f"author: {self.yt.author}") 
-        # print(self.yt.channel_url)     # 影片作者頻道網址
-        # print(self.yt.thumbnail_url)   # 影片縮圖網址
-        # print(self.yt.views)           # 影片觀看數
-
-    def check_default_value(self):
-        if self.link is None or self.link == "":
+        # check if the resolution is valid
+        allowed_resolution_list = ["highest", "1080p", "720p", "360p", "240p", "120p"]
+        if self.resolution not in allowed_resolution_list:
             self.is_error = True
-            self.error_msg = 'link is empty'
+            self.error_msg = f'resolution should be one of options: {",".join(allowed_resolution_list)}. Find {self.format}' 
 
-        allowede_format_list = ["highest", "1080p", "720p", "360p", "240p", "120p"]
-        if self.format not in allowede_format_list:
+
+    def reset_file_name(self):
+        if self.file_name is None or self.file_name == "":
+            self.file_name = self.yt.title
+
+
+    def onProgress(self, stream, chunk, remains):
+        total = stream.filesize                     # get file size
+        percent = (total-remains) / total * 100     # calculate the remain percentage
+        msg = f'Downloading… {percent:05.2f}%'
+        print(msg)
+        # self.display_msg += f"{msg}\n"
+        # st.session_state["text_area"] = f"{self.display_msg}"
+        if self.progress_bar is not None:
+            self.progress_bar.progress((total-remains) / total, text=msg)
+
+
+    def connect_yt(self):
+        try:
+            self.yt = YouTube(self.link, on_progress_callback=self.onProgress)
+            self.reset_file_name()
+
+            self.display_msg += f"Title: {self.yt.title} \nlength of the video: {self.yt.length} s \nAuthor: {self.yt.author} \n"
+            print(f"title: {self.yt.title}")
+            print(f"length of the video: {self.yt.length}") 
+            print(f"author: {self.yt.author}") 
+            # print(self.yt.channel_url)     # 影片作者頻道網址
+            # print(self.yt.thumbnail_url)   # 影片縮圖網址
+            # print(self.yt.views)           # 影片觀看數
+        except Exception as e:
             self.is_error = True
-            self.error_msg = f'format should be one of options: {",".join(allowede_format_list)}. Find {self.format}' 
+            self.error_msg = e
+            print(e)
 
-        if self.file_name
 
     def download(self):
-        print('Start downloading!')
-        filename = f'./{self.default_folder}/{self.yt.title}.{self.format}'
+        filename = f'./{self.default_folder}/{self.file_name}.{self.format}'
+        msg = f"Download to {filename}"
+        print(msg)
+        self.display_msg += f"{msg}\n"
+        
         try:
             if format == 'mp3':
                 self.yt.streams.filter(only_audio=True)\
@@ -61,9 +98,12 @@ class DownloadYoutube():
                     .download(filename=filename)
                 
             print('Done!')
+            self.display_msg += "Done!\n"
             print('-'*50)
         except Exception as e:
-            raise SystemError(e)
+            self.is_error = True
+            self.error_msg = e
+            print(e)
 
 
 if __name__ == "__main__":
